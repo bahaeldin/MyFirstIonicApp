811a49601cb4c96f11cb76f8ec0294a385067e6a
BAHAELDIN BAHAR <36114314+bahaeldin@users.noreply.github.com>
2021-07-08 06:58:37 +0200

    converted to javaScript

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import Home from '../views/Home.vue'
 
 const routes = [{
     path: '/',
     redirect: '/home'
-}, {
-    path: '/home',
-    name: 'Home',
-    component: Home
 }]
 
 const router = createRouter({
